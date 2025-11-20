f5b1107fe38db58068b70745473fe2c17d301808
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-20 20:59:36 +0300

    Update py_bridge.py

--- languages/python/py_bridge.py
+++ languages/python/py_bridge.py
@@ -1,1 +1,20 @@
+import socket
+import json
 
+def handle_request(data):
+    cmd = data['cmd']
+    if cmd == 'analyze_image':
+        return {'result': 'faces_found'}
+    return {'error': 'unknown_cmd'}
+
+# Сервер для приёма запросов от Java
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('localhost', 8080))
+server.listen(1)
+
+while True:
+    conn, addr = server.accept()
+    data = json.loads(conn.recv(1024))
+    response = handle_request(data)
+    conn.send(json.dumps(response).encode())
+    conn.close()

d72f18fab147a55d52a0391b21c744e59f94711e
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-20 20:55:30 +0300

    Update native_utils.h

--- languages/c/native_utils.h
+++ languages/c/native_utils.h
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+// Пример функции для работы с памятью
+void* allocate_buffer(int size) {
+    return malloc(size);
+}
+
+// Функция для криптографии (упрощённо)
+void encrypt_data(char* data, int key) {
+    for (int i = 0; i < strlen(data); i++) {
+        data[i] ^= key;
+    }
+}

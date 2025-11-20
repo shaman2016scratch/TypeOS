806da4f78fa7e71ddd33e67b47f2806daf261551
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-20 20:43:06 +0300

    Update BackgroundService.java

--- android/app/src/main/java/com/typeos/services/BackgroundService.java
+++ android/app/src/main/java/com/typeos/services/BackgroundService.java
@@ -1,1 +1,12 @@
+package com.typeos.services;
 
+import android.app.Service;
+import android.content.Intent;
+
+public class BackgroundService extends Service {
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Фоновые задачи (резервное копирование, мониторинг)
+        return START_STICKY;
+    }
+}

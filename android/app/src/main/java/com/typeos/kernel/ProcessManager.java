4bbf6162471a58b9fd04a3cc498dd2c502752650
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-20 20:33:00 +0300

    Create ProcessManager.java

---
+++ android/app/src/main/java/com/typeos/kernel/ProcessManager.java
@@ -0,0 +1,9 @@
+package com.typeos.kernel;
+
+public class ProcessManager {
+    public void startProcess(String cmd, String lang) {
+        // Запуск процесса (Java/Python/C/JS)
+    }
+
+    public void killProcess(int pid) { /* ... */ }
+}
